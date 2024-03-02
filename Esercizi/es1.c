accfd9c57653efd448568d51a3a201b6e26eda40
Pierluca <grassopierluca@alice.it>
2024-03-02 17:42:51 +0100

    .

--- Esercizi/es1.c
+++ Esercizi/es1.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 typedef struct {
     int giorno;
@@ -8,9 +10,14 @@ typedef struct {
 } Data;
 
 
-void ConvertToData(Data * data,char* dataStr){
-    sscanf(dataStr,"%d/%d/%d",&data->giorno,&data->mese,&data->anno);
+Data CalculateNewDate(Data data1,Data data2){
+    Data ris;
+    
+    ris.giorno =floor((data1.giorno + data2.giorno)/2);
+    ris.mese = floor((data1.mese + data2.mese)/2);
+    ris.anno = floor((data1.anno + data2.anno)/2);
 
+    return ris;
 }
 
 int checkFormato(char* data){
@@ -28,19 +35,16 @@ int checkFormato(char* data){
         formato = 0;
     }
 
-     if (data[3] < '0' || data[3] > '9'){
+    if (data[3] < '0' || data[3] > '9'){
         formato = 0;
     }
-
      if (data[4] < '0' || data[4] > '9'){
         formato = 0;
     }
-
      if (data[5] != '/'){
         formato = 0;
     }
-
-     if (data[6] < '0' || data[6] > '9'){
+    if (data[6] < '0' || data[6] > '9'){
         formato = 0;
     }
      if (data[7] < '0' || data[7] > '9'){
@@ -66,29 +70,38 @@ int main(){
    
     int formato = 0;
     while (formato == 0){
-        printf("inserisci la data 1: ");
+       printf("Inserisci la prima data nel formato gg/mm/aaaa: ");
         scanf("%s",dataStr1);
-        //formato = checkFormato(dataStr1);   
-        formato = 1;
+        formato = checkFormato(dataStr1);
+        if(strlen(dataStr1)<8) formato = 0;
+        if(formato == 0) printf("\nReinserisci la prima data,c'e' un errore nel formato\n");
     }
 
-   formato = 0;
 
-    /*while (formato == 0){
-        printf("inserisci la data 2: ");
+    printf("\n\n");
+    formato = 0;
+
+    while (formato == 0){
+        printf("Inserisci la seconda data nel formato gg/mm/aaaa: ");
         scanf("%s",dataStr2);
-        formato = checkFormato(dataStr2);   
-    }*/
+        formato = checkFormato(dataStr2);  
+        if(strlen(dataStr1)<8) formato = 0;
+        if(formato == 0) printf("\nReinserisci la seconda data,c'e' un errore nel formato\n");
+    }
 
     sscanf(dataStr1,"%d/%d/%d",&data1.giorno,&data1.mese,&data1.anno);
     sscanf(dataStr2,"%d/%d/%d",&data2.giorno,&data2.mese,&data2.anno);
 
    
+    Data ris = CalculateNewDate(data1,data2);
 
-    printf("%d\n",data1.giorno); 
-    printf("%d\n",data1.mese);
-    printf("%d\n",data1.anno);
+    
+    
+    
+    printf("%d/%d/%d",ris.giorno,ris.mese,ris.anno);
 
+    
+     
     
 
 }
